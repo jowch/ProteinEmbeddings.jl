06737a45ad0c205d2e9c9bd8911d93adcd4f4683
Jonathan Chen <jwhc@ucla.edu>
2023-03-24 12:38:34 -0700

    -gpu

--- src/ProteinEmbeddings.jl
+++ src/ProteinEmbeddings.jl
@@ -34,19 +34,12 @@ function __init__()
             if model == "esm1b_t33_650M_UR50S":
                 self.model, self.alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
                 self.batch_converter = self.alphabet.get_batch_converter()
-                self.embedding_dims = 1280
             elif model == "esm2_t33_650M_UR50D":
                 self.model, self.alphabet = esm.pretrained.esm2_t33_650M_UR50D()
                 self.batch_converter = self.alphabet.get_batch_converter()
-                self.embedding_dims = 1280
 
-            # self.use_gpu = torch.cuda.is_available()
-            # self.device = "cuda" if self.use_gpu else "cpu"
             self.device = "cpu"
             self.model.eval()
-
-            # if self.use_gpu:
-            #     self.model = self.model.cuda()
 
         def embed(self, seqs: List[str]):
             batch_labels, batch_strs, batch_tokens = self.batch_converter(seqs)
@@ -54,8 +47,6 @@ function __init__()
 
             # Extract per-residue representations (on CPU)
             with torch.no_grad():
-                # if self.use_gpu:
-                #     batch_tokens = batch_tokens.to(device = self.device, non_blocking=True)
                 results = self.model(batch_tokens, repr_layers=[33], return_contacts=False)
 
             token_representations = results["representations"][33].numpy()
